b24c478a532b497392cf3f7c61f805b7c637f26a
Bharat Nagda <nagdabharat@gmail.com>
2020-06-09 10:48:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+// Add your functions and code here 
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
 
-// Add your functions and code here
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten()  {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+  return([name, ...kittens]);
+}
