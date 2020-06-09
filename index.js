33598086f4e8b0900ef2f65984c6e9308616b953
Bharat Nagda <nagdabharat@gmail.com>
2020-06-09 10:49:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,9 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
+  return([...kittens, name]);
+}
+
+function prependKitten(name) {
   return([name, ...kittens]);
 }
